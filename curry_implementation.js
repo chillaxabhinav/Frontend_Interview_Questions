fb8dad833b13b8b5d8283d188314ce8a0ca4634e
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-15 17:52:37 +0530

    curry implementation

--- curry_implementation.js
+++ curry_implementation.js
@@ -1,3 +1,23 @@
 /*
 f(a, b, c) = f(a)(b)(c)
 */
+
+const convertCurry = (fn) => {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn(...args);
+        } else {
+            return function(...next) {
+                return curried(...args, ...next);
+            }
+        }
+    }
+};
+
+const sum = (a, b, c) => {
+    return a + b + c
+};
+
+const convertedCurry = convertCurry(sum);
+
+console.log(convertedCurry(1)(2)(3))
