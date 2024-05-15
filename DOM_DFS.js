20bcd3bd97eea3dfbbaf10212ced38676d226fe6
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-15 22:50:43 +0530

    changes

--- DOM_DFS.js
+++ DOM_DFS.js
@@ -1,6 +1,8 @@
 // DOM DFS Inorder Traversal
 // Everything is Node, But Element is special type of Node
 
+// If you want to know that node is equal or not then. There is a function on each Node i.e. isEqualNode(toCompare)
+
 function DfsTraversal(parent) {
     const children = parent.children;
     if (children && children.length === 0) return;
