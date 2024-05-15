9ff1ca84ab31c07034cc19233358294e8dd0db89
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-15 17:20:50 +0530

    infinite curry

--- curry_infinite.js
+++ curry_infinite.js
@@ -2,3 +2,14 @@
 sum(1)(2)(3)(4)(5)(6)(7)(8)()
 */
 
+function sum(a) {
+    return function(b) {
+        if (b === undefined) {
+            return a;
+        } else {
+            return sum(a+b);
+        }
+    }
+}
+
+console.log(sum(1)(2)(4)(5)(6)(7)(8)())
