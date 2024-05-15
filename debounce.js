acfe12c09365521575cf8f8a25059c6c569caf62
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-15 18:53:06 +0530

    debounce

---
+++ debounce.js
@@ -0,0 +1,11 @@
+function debounce(fn, wait) {
+    let timer;
+    return function(...args) {
+        if (timer) {
+            clearTimeout(timer);
+            timer = setTimeout(() => fn(...args), wait);
+        } else {
+            timer = setTimeout(() => fn(...args), wait);
+        }
+    }
+}
