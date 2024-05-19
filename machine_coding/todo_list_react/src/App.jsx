63eb84109095f3f0773e8f88fdb89f4c425fe8cf
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-19 16:34:55 +0530

    changes

--- machine_coding/todo_list_react/src/App.jsx
+++ machine_coding/todo_list_react/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { nanoid } from 'nanoid';
+
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const inputRef = useRef(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	/*
+		{
+			text: "",
+			id: "" // nanoId
+		}
+	*/
+	const [todos, setTodos] = useState([]);
+	const [mode, setMode] = useState(false);
+
+	const addTodos = () => {
+		const currentText = inputRef.current.value;
+		if (currentText === "") {
+			alert("Please add valid task");
+			return
+		}
+		const newTodo = {
+			text: currentText,
+			id: nanoid()
+		}
+		const newTodoList = todos.slice().map(ele => structuredClone(ele));
+		// add todo at top
+		newTodoList.unshift(newTodo);
+		setTodos(newTodoList);
+		inputRef.current.value = "";
+	};
+
+	const onClickTodo = (e) => {
+		const currentEle = e.target;
+		if (currentEle.tagName === 'BUTTON') {
+			const todoId = currentEle.parentElement.getAttribute('data-index');
+			if (currentEle.innerText === 'Delete') {
+				const filteredTodos = todos.filter(todo => todo.id !== todoId);
+				setTodos(filteredTodos);
+			}
+		}
+	}
+
+	return (
+		<>
+			<div>
+				<h1>Todo List React</h1>
+				<input type='text' placeholder='Write some task' ref={inputRef} />
+				<button onClick={() => addTodos()}>Add Todo</button>
+			</div>
+			<ul onClick={(e) => onClickTodo(e)}>
+				{todos.map((todo) => {
+					const { text, id } = todo;
+					return (
+						<li key={id} data-index={id}>
+							<span>{text}</span>
+							<button>Edit</button>
+							<button>Delete</button>
+						</li>
+					)
+				})}
+			</ul>
+		</>
+	);
 }
 
 export default App
