3c157ed8a3765d6fd11709bf8e09fc93be7d57b2
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-26 01:36:47 +0530

    infinite scroll

--- JS_Implementation/throttle.js
+++ JS_Implementation/throttle.js
@@ -3,15 +3,18 @@ Throttling helps us to limit the function call for a certain amount of time
 After that we can limit only the function can be called
 */
 
-function throttle(fn, limit) {
-    let lastTime = new Date().getTime();
-    return function(...args) {
-        const currentTime = new Date().getTime();
-        if ((currentTime - lastTime) > limit) {
-            lastTime = new Date().getTime();
-            fn(...args);
-        }
-    }
+function throttle(mainFunction, delay) {
+    let timerFlag = null; // Variable to keep track of the timer
+  
+    // Returning a throttled version 
+    return (...args) => {
+      if (timerFlag === null) { // If there is no timer currently running
+        mainFunction(...args); // Execute the main function 
+        timerFlag = setTimeout(() => { // Set a timer to clear the timerFlag after the specified delay
+          timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
+        }, delay);
+      }
+    };
 }
 
 function test() {
--- machine_coding/infinite_scroll_react/src/App.jsx
+++ machine_coding/infinite_scroll_react/src/App.jsx
@@ -1,7 +1,82 @@
+/*
+	Implement Infinite Scroll in React
+
+	Requirements
+		- Implement Infinite Scrolling for fetching more products when the user reaches the bottom of the page.
+		- Ensure that loading indicators are displayed appropriately while fetching data.
+		- Implement optimizations to prevent excessive API requests during scrolling.
+*/
+
+import { useState, useEffect } from 'react';
+
+function throttle(mainFunction, delay) {
+	let timerFlag = null;
+	return (...args) => {
+		if (timerFlag === null) {
+			mainFunction(...args);
+			timerFlag = setTimeout(() => {
+			timerFlag = null;
+			}, delay);
+		}
+	};
+}
+
 function App() {
+	const [products, setProducts] = useState([]);
+	const [page, setPage] = useState(1);
+
+	const [loading, setLoading] = useState(false);
+
+	const fetchProducts = async () => {
+		try {
+			setLoading(true);
+			const resp = await fetch(`https://dummyjson.com/products?limit=${page*10}`);
+			const data = await resp.json();
+			setPage((prev) => prev + 1);
+			setProducts(data);
+		} catch (e) {
+			console.log('error in fetching --> ', e);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	const handleScroll = throttle(() => {
+		if (
+			(window.innerHeight + document.documentElement.scrollTop + 500) > document.documentElement.offsetHeight
+			&& !loading
+		) {
+			fetchProducts();
+		}
+	}, 500);
+
+	useEffect(() => {
+		document.addEventListener('scroll', handleScroll);
+		return () => document.removeEventListener('scroll', handleScroll);
+	}, [handleScroll]);
+
+	const { products: allProducts } = products;
+
 	return (
-		<>
-		</>
+		<div>
+			<h1>Infinite Scroll</h1>
+			{allProducts?.length > 0 && (
+				<div className='products'>
+					{allProducts?.map(product => {
+						return (
+							<div className='product__single' key={product.id}>
+								<img src={product.thumbnail} alt={product.title} />
+								<span>{product.title}</span>
+							</div>
+						)
+					})}
+				</div>
+			)}
+		</div>
 	);
 };
 
