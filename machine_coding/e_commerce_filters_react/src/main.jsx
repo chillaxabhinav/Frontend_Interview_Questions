0694ccc87ce119c23e760cb0a5b2792607ceec40
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-27 19:15:39 +0530

    changes

--- machine_coding/e_commerce_filters_react/src/main.jsx
+++ machine_coding/e_commerce_filters_react/src/main.jsx
@@ -1,3 +1,18 @@
+/* Design and Implement E-Commerce filters using React.JS
+
+Requirements
+- Fetch a list of products and render them on the page.
+- Add a filter to sort the products by price.
+- Add a filter to show/hide out of stock products.
+- Ability to search products.
+- Add a filter to show products above certain rating.
+- Production grade state management
+
+Bonus
+- Ability to share the state of page ( applied filters ) with other users.
+- Implement pagination with respect to filters applied.
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
