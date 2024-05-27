1213b39cc93dbec82c2a2a8f6bbd4c7070954271
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-27 19:30:14 +0530

    changes

--- machine_coding/e_commerce_filters_react/src/pages/appLayout.jsx
+++ machine_coding/e_commerce_filters_react/src/pages/appLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
-        <div className='flex flex-row items-center justify-between p-4'>
-            <div>E Commerce Store</div>
+        <div className='flex flex-row items-center justify-between p-4 font-mono'>
+            <div className='text-2xl'>E-Commerce Store</div>
             <div>
-                <input type='text' placeholder='Search Product here'/>
+                <input type='text' placeholder='Search Product...' className='text-2xl'/>
             </div>
             <Outlet />
         </div>
