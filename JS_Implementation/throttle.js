38c8468fc7ca1642f1e9cb71d187716df03c3f78
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-29 18:05:25 +0530

    changes

--- JS_Implementation/throttle.js
+++ JS_Implementation/throttle.js
@@ -1,21 +1,12 @@
-/*
-Throttling helps us to limit the function call for a certain amount of time
-After that we can limit only the function can be called
-*/
-
-function throttle(mainFunction, delay) {
-    let timerFlag = null; // Variable to keep track of the timer
-  
-    // Returning a throttled version 
-    return (...args) => {
-      if (timerFlag === null) { // If there is no timer currently running
-        mainFunction(...args); // Execute the main function 
-        timerFlag = setTimeout(() => { // Set a timer to clear the timerFlag after the specified delay
-          timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
-        }, delay);
-      }
-    };
+function throttle(fn, wait) {
+	let timer = null;
+	return function (...params) {
+		if (timer !== null) return;
+		fn(...params);
+		timer = setTimeout(() => timer = null, wait);
+	};
 }
+
 
 function test() {
     console.log("Testing throttle");
