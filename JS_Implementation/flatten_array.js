e518bd610230c6780cb74e6140032313568cbf5d
Hari Shankar <harishankar@Haris-MacBook-Air.local>
2024-05-29 18:10:37 +0530

    changes

--- JS_Implementation/flatten_array.js
+++ JS_Implementation/flatten_array.js
@@ -1,14 +1,19 @@
-function doArrayFlat(arr) {
-    if (!Array.isArray(arr)) return [arr];
-    const output = [];
-    for (let i=0; i < arr.length; i++) {
-        const outputArray = doArrayFlat(arr[i]);
-        output.push.apply(output, outputArray);
-    }
-    return output;
+function flattenArray(arr) {
+	const output = [];
+	if (!Array.isArray(arr) && arr.length === 0) return output;
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		if (Array.isArray(element)) {
+			const flatten = flattenArray(element);
+			output.push.apply(output, flatten);
+		} else {
+			output.push(element);
+		}
+	}
+	return output;
 }
 
 const arr = [1, [2, 3], 4, [5, 6, 7]];
 
-console.log(doArrayFlat(arr));
+console.log(flattenArray(arr));
 
